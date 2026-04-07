4a7e8cab6977f225a2779d116a3b16c9b44675d3
sherzod-uralov <sherzoduralov432@gmail.com>
2026-04-07 14:23:55 +0500

    Auto freeze DOCX→PDF on workflow create, remove separate start-workflow endpoint

--- src/modules/workflow/workflow.module.ts
+++ src/modules/workflow/workflow.module.ts
@@ -5,10 +5,11 @@ import { WorkflowController } from './workflow.controller'
 import { NotificationModule } from '../notification/notification.module'
 import { TelegramModule } from '../telegram/telegram.module'
 import { AuditLogModule } from '../audit-log/audit-log.module'
+import { MinioService } from '@clients'
 
 @Module({
   imports: [PrismaModule, NotificationModule, TelegramModule, AuditLogModule],
-  providers: [WorkflowService],
+  providers: [WorkflowService, MinioService],
   controllers: [WorkflowController],
 })
 export class WorkflowModule {}
